a516d03391baae40fbb723fd737551eeb442aaf0
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-13 14:16:13 -0600

    created files to hold objects

---
+++ src/utils/componentObjects.js
@@ -0,0 +1,13 @@
+import picture from '../assets/kevin.jpg';
+
+const testimonialCardProps = {
+  image: {
+    srcImg: picture,
+    alt: 'Kevin Castro at the Sand Dunes',
+  },
+  title: 'Kevin Castro',
+  username: '@kevincastro',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+};
+
+export { testimonialCardProps };
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const COLORS = {
+  secondaryText: '#525252',
+};
+
+export { COLORS };
