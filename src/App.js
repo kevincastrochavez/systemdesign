2de7057659a28ab6ca60bb64ce6d5e942b65a14f
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-13 14:21:37 -0600

    imported component in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
+import TestimonialCard from './components/testimonialCard/TestimonialCard';
+import { testimonialCardProps } from './utils/componentObjects';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<h1>Home</h1>} />
+        <Route
+          path='/testimonialCard'
+          element={<TestimonialCard {...testimonialCardProps} />}
+        />
       </Routes>
     </BrowserRouter>
   );
