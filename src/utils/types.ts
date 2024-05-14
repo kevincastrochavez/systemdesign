248cc4ef22f75a163766aa79da46d3d92d783e35
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-13 20:33:37 -0600

    added comments for storybook docs

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,9 +1,21 @@
 export interface TestimonialCardProps {
+  /**
+   * Image source and alt text
+   */
   image?: {
     srcImg: string;
     alt: string;
   };
+  /**
+   * Title of the card
+   */
   title: string;
+  /**
+   * Username of the card
+   */
   username?: string;
+  /**
+   * Text of the card
+   */
   text: string;
 }
