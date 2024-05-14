7ce9de87ee9170f83ab27ea6e2e56b5cb2c8aee8
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-13 18:06:24 -0600

    created mocks for the testimonial card

---
+++ src/components/__mocks__/testimonialCardMocks.js
@@ -0,0 +1,32 @@
+import picture from '../../assets/kevin.jpg';
+
+const testimonialCardComplete = {
+  image: {
+    srcImg: picture,
+    alt: 'Kevin Castro at the Sand Dunes',
+  },
+  title: 'Kevin Castro Chavez Juan Francisco',
+  username: '@kevincastro',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+};
+
+const testimonialCardWithoutImage = {
+  title: 'Kevin Castro Chavez Juan Francisco',
+  username: '@kevincastro',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+};
+
+const testimonialCardWithoutUsername = {
+  image: {
+    srcImg: picture,
+    alt: 'Kevin Castro at the Sand Dunes',
+  },
+  title: 'Kevin Castro Chavez Juan Francisco',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+};
+
+export {
+  testimonialCardComplete,
+  testimonialCardWithoutImage,
+  testimonialCardWithoutUsername,
+};
