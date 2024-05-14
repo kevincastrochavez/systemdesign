ddaf867fb036063329dbaf2202d720f9e87ca5c2
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-14 13:32:09 -0600

    created stories for the chip component

---
+++ src/components/chip/Chip.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta } from '@storybook/react';
+
+import Chip from './Chip';
+import { ChipProps } from '../../utils/types';
+import { chipDefault, chipLong } from '../__mocks__/chipMocks';
+
+// eslint-disable-next-line storybook/story-exports
+const meta: Meta<typeof Chip> = {
+  title: 'Components/Chip',
+  component: Chip,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Basic = (args: ChipProps) => <Chip {...args} />;
+Basic.args = {
+  ...chipDefault,
+};
+
+export const LongChip = (args: ChipProps) => <Chip {...args} />;
+LongChip.args = {
+  ...chipLong,
+};
