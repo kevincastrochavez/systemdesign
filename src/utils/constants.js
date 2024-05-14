29ae4f6a7f895a7ae3a3789d41832202ff03d104
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-14 13:30:22 -0600

    added new colors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,9 @@ const COLORS = {
   disabledText: '#A3A3A3',
   indigo700: '#4338CA',
   indigo800: '#3730A3',
+  green700: '#15803D',
+  green200: '#BBF7D0',
+  green50: '#F0FDF4',
 };
 
 export { COLORS };
