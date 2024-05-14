79e9fe9c658a33ca87b3125c49c9a763080d3c87
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-14 13:07:03 -0600

    added interface for more button

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 export interface TestimonialCardProps {
   /**
    * Image source and alt text
@@ -18,4 +20,23 @@ export interface TestimonialCardProps {
    * Text of the card
    */
   text: string;
+}
+
+export interface MoreButtonProps {
+  /**
+   * Link to be redirected to
+   */
+  to: string;
+  /**
+   * Text to be displayed
+   */
+  text: string;
+  /**
+   * Icon to be displayed
+   */
+  icon?: ReactNode;
+  /**
+   * Whether the button is disabled
+   */
+  disabled?: boolean;
 }
