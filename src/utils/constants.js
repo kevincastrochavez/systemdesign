80d17bff887aeedb87e9e7d96477033a67e07857
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-15 14:08:33 -0600

    added new colors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,20 @@
 const COLORS = {
   secondaryText: '#525252',
   disabledText: '#A3A3A3',
-  indigo700: '#4338CA',
+  grayBackground: '#858e96',
   indigo800: '#3730A3',
+  indigo700: '#4338CA',
+  indigo200: '#c7d2fe',
+  indigo50: '#eef2ff',
   green700: '#15803D',
   green200: '#BBF7D0',
   green50: '#F0FDF4',
-  grayBackground: '#858e96',
+  orange700: '#c2410c',
+  orange200: '#fed7aa',
+  orange50: '#fff7ed',
+  cyan700: '#0e7490',
+  cyan200: '#a5f3fc',
+  cyan50: '#ecfeff',
 };
 
 export { COLORS };
