b96310329d342fb871a9194f85c5c8632ee21906
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-15 14:09:18 -0600

    added test to chip changes

--- src/components/chip/Chip.test.tsx
+++ src/components/chip/Chip.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 
 import Chip from './Chip';
-import { chipDefault, chipLong } from '../__mocks__/chipMocks';
+import { chipDefault, chipLong, chipCyan } from '../__mocks__/chipMocks';
 
 describe('Chip', () => {
   it('renders the text passed to it', () => {
@@ -10,6 +10,15 @@ describe('Chip', () => {
     const chip = screen.getByText(chipDefault.text);
     expect(chip).toBeInTheDocument();
     expect(chip).toHaveTextContent('Interior');
+  });
+
+  it('applies correct styles for different colors', () => {
+    render(<Chip {...chipCyan} />);
+
+    const chip = screen.getByText(chipCyan.text);
+    expect(chip).toHaveStyle({ color: '#0e7490' });
+    expect(chip).toHaveStyle({ borderColor: '#a5f3fc' });
+    expect(chip).toHaveStyle({ backgroundColor: '#ecfeff' });
   });
 
   it('renders the long text passed to it', () => {
