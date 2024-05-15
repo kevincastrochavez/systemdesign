3820017f428beba72071efa68c9159eb6d28277f
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-15 14:34:10 -0600

    modified the more button to accept colors

--- src/components/moreButton/MoreButton.tsx
+++ src/components/moreButton/MoreButton.tsx
@@ -6,7 +6,6 @@ import { MoreButtonProps } from '../../utils/types';
 import { COLORS } from '../../utils/constants';
 
 const linkCss = css`
-  color: ${COLORS.indigo700};
   transition: color 0.3s ease-in-out;
   text-decoration: none;
   display: flex;
@@ -18,12 +17,47 @@ const linkCss = css`
     width: 20px;
   }
 
-  &:hover {
-    color: ${COLORS.indigo800};
+  &:hover svg {
+    transform: translateX(4px);
+  }
 
-    svg {
-      transform: translateX(4px);
+  &[data-color='green'] {
+    color: ${COLORS.green700};
+    &:hover {
+      color: ${COLORS.green800};
+      svg {
+        color: ${COLORS.green800};
+      }
+    }
+  }
+
+  &[data-color='indigo'] {
+    color: ${COLORS.indigo700};
+    &:hover {
       color: ${COLORS.indigo800};
+      svg {
+        color: ${COLORS.indigo800};
+      }
+    }
+  }
+
+  &[data-color='orange'] {
+    color: ${COLORS.orange700};
+    &:hover {
+      color: ${COLORS.orange800};
+      svg {
+        color: ${COLORS.orange800};
+      }
+    }
+  }
+
+  &[data-color='cyan'] {
+    color: ${COLORS.cyan700};
+    &:hover {
+      color: ${COLORS.cyan800};
+      svg {
+        color: ${COLORS.cyan800};
+      }
     }
   }
 `;
@@ -38,13 +72,24 @@ const disabledLinkCss = css`
  * @param {String} to - link to be redirected to
  * @param {String} text - text to be displayed
  * @param {ReactNode} icon - icon to be displayed
+ * @param {String} color - color of the button ('green' | 'indigo' | 'orange' | 'cyan')
  * @param {Boolean} disabled - whether the button is disabled
  * @returns {JSX.Element}
  */
 /** Primary UI component for the More Button component */
-function MoreButton({ to, text, icon, disabled = false }: MoreButtonProps) {
+function MoreButton({
+  to,
+  text,
+  icon,
+  color = 'indigo',
+  disabled = false,
+}: MoreButtonProps) {
   return (
-    <Link to={to} css={[linkCss, disabled && disabledLinkCss]}>
+    <Link
+      to={to}
+      css={[linkCss, disabled && disabledLinkCss]}
+      data-color={color}
+    >
       {text} {icon}
     </Link>
   );
