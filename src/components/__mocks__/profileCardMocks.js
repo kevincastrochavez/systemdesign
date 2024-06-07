48c15a79425aaebe77b4ffed3c1ad1ab1f954478
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-06-07 13:44:40 -0600

    added mocks and storybook story for the profile card component

---
+++ src/components/__mocks__/profileCardMocks.js
@@ -0,0 +1,47 @@
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandInstagram,
+  IconBrandX,
+} from '@tabler/icons-react';
+
+import picture from '../../assets/kevin.jpg';
+
+const profileCardDefault = {
+  image: {
+    src: picture,
+    alt: 'Kevin Castro at the Sand Dunes',
+  },
+  title: 'Kevin Castro',
+  subtitle: 'FrontEnd Engineer @ Tesla',
+  text: 'You will never find an engineer like me. This is your chance. Hire me now or someone else will!',
+  button: {
+    text: 'Contact Me',
+  },
+  socialBar: {
+    actionIcons: [
+      {
+        icon: <IconBrandGithub />,
+        variant: 'transparent',
+        onClick: () => {},
+      },
+      {
+        icon: <IconBrandLinkedin />,
+        variant: 'transparent',
+        onClick: () => {},
+      },
+      {
+        icon: <IconBrandInstagram />,
+        variant: 'transparent',
+        onClick: () => {},
+      },
+      {
+        icon: <IconBrandX />,
+        variant: 'transparent',
+        onClick: () => {},
+      },
+    ],
+  },
+};
+
+export { profileCardDefault };
---
+++ src/components/profileCard/ProfileCard.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta } from '@storybook/react';
+
+import ProfileCard from './ProfileCard';
+import { ProfileCardProps } from '../../utils/types';
+import { profileCardDefault } from '../__mocks__/profileCardMocks';
+
+// eslint-disable-next-line storybook/story-exports
+const meta: Meta<typeof ProfileCard> = {
+  title: 'Components/ProfileCard',
+  component: ProfileCard,
+  argTypes: {},
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Basic = (args: ProfileCardProps) => <ProfileCard {...args} />;
+Basic.args = {
+  ...profileCardDefault,
+};
