9033dfafd6c1a9f2b6dc95ff678da50d1034d8cb
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-06-07 11:44:22 -0600

    created mocks and props for the action icon

---
+++ src/components/__mocks__/actionIconMocks.js
@@ -0,0 +1,10 @@
+import { IconBrandGithub } from '@tabler/icons-react';
+
+const actionIconDefault = {
+  variant: 'default',
+  color: 'indigo',
+  icon: <IconBrandGithub />,
+  onClick: () => {},
+};
+
+export { actionIconDefault };
--- src/utils/componentObjects.js
+++ src/utils/componentObjects.js
@@ -1,26 +1,23 @@
-import { IconArrowRight } from '@tabler/icons-react';
+import { IconArrowRight, IconBrandGithub } from '@tabler/icons-react';
 
 import picture from '../assets/kevin.jpg';
 import homePicture from '../assets/home.jpg';
 
-const testimonialCardProps = {
-  image: {
-    srcImg: picture,
-    alt: 'Kevin Castro at the Sand Dunes',
-  },
-  title: 'Kevin Castro Chavez',
-  username: '@kevincastro',
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+const actionIconProps = {
+  variant: 'outline',
+  color: 'indigo',
+  size: 'xl',
+  radius: 'sm',
+  disabled: false,
+  icon: <IconBrandGithub />,
+  onClick: () => {},
 };
 
-const moreButtonProps = {
-  to: '/someLink',
-  text: 'Read More',
-  icon: <IconArrowRight />,
-};
-
-const chipProps = {
-  text: 'Interior',
+const avatarProps = {
+  src: picture,
+  alt: 'Kevin Castro at the Sand Dunes',
+  size: 'md',
+  radius: 'full',
 };
 
 const blogCardProps = {
@@ -33,13 +30,6 @@ const blogCardProps = {
   ctaLink: '/someLink',
   ctaIcon: <IconArrowRight />,
   chips: ['Interior', 'Living Room'],
-};
-
-const avatarProps = {
-  src: picture,
-  alt: 'Kevin Castro at the Sand Dunes',
-  size: 'md',
-  radius: 'full',
 };
 
 const buttonProps = {
@@ -55,11 +45,32 @@ const buttonProps = {
   icon: <IconArrowRight />,
 };
 
+const chipProps = {
+  text: 'Interior',
+};
+
+const moreButtonProps = {
+  to: '/someLink',
+  text: 'Read More',
+  icon: <IconArrowRight />,
+};
+
+const testimonialCardProps = {
+  image: {
+    srcImg: picture,
+    alt: 'Kevin Castro at the Sand Dunes',
+  },
+  title: 'Kevin Castro Chavez',
+  username: '@kevincastro',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+};
+
 export {
-  testimonialCardProps,
-  moreButtonProps,
-  chipProps,
-  blogCardProps,
+  actionIconProps,
   avatarProps,
+  blogCardProps,
   buttonProps,
+  chipProps,
+  moreButtonProps,
+  testimonialCardProps,
 };
