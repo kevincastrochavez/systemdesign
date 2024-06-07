4582fb0e2947f1adcf0a954bc2b5daa1350d5cd8
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-06-07 11:43:44 -0600

    ordered imports and added action icon component to app

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import ChipWrapper from './components/chip/ChipWrapper';
 import BlogCardWrapper from './components/blogCard/BlogCardWrapper';
 import AvatarWrapper from './components/avatar/AvatarWrapper';
 import ButtonWrapper from './components/button/ButtonWrapper';
+import ProfileCardWrapper from './components/profileCard/ProfileCardWrapper';
+import ActionIconWrapper from './components/actionIcon/ActionIconWrapper';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/testimonialCard' element={<TestimonialCardWrapper />} />
-        <Route path='/moreButton' element={<MoreButtonWrapper />} />
-        <Route path='/chip' element={<ChipWrapper />} />
-        <Route path='/blogCard' element={<BlogCardWrapper />} />
+        <Route path='/actionIcon' element={<ActionIconWrapper />} />
         <Route path='/avatar' element={<AvatarWrapper />} />
+        <Route path='/blogCard' element={<BlogCardWrapper />} />
         <Route path='/button' element={<ButtonWrapper />} />
+        <Route path='/chip' element={<ChipWrapper />} />
+        <Route path='/moreButton' element={<MoreButtonWrapper />} />
+        <Route path='/testimonialCard' element={<TestimonialCardWrapper />} />
       </Routes>
     </BrowserRouter>
   );
