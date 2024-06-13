5cf0e522d9065871970c033e0fe3566f69173eed
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-06-13 12:05:29 -0600

    created the gallery component

---
+++ src/components/gallery/Gallery.tsx
@@ -0,0 +1,80 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useState } from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+import { GalleryProps } from '../../utils/types';
+import { COLORS } from '../../utils/constants';
+
+const galleryContainerCss = css`
+  width: 100%;
+  max-width: 700px;
+  overflow: hidden;
+
+  & > img {
+    border-radius: 10px;
+    width: 100%;
+    height: 400px;
+    object-fit: cover;
+  }
+`;
+
+const thumbnailContainerCss = css`
+  margin-top: 24px;
+  display: flex;
+  gap: 16px;
+  overflow-x: scroll;
+
+  & img {
+    border-radius: 8px;
+    height: 120px;
+    min-width: 80px;
+    object-fit: cover;
+    object-position: bottom;
+    cursor: pointer;
+
+    &[data-selected='true'] {
+      border: 3px solid ${COLORS.indigo600};
+    }
+
+    @media (min-width: 500px) {
+      min-width: 188px;
+      height: 190px;
+    }
+  }
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+function Gallery({ images }: GalleryProps) {
+  const [picturedShown, setPicturedShown] = useState(0);
+  const mainImage = images[picturedShown];
+
+  return (
+    <div css={galleryContainerCss}>
+      <img src={mainImage.src} alt={mainImage.alt} />
+
+      {images.length > 1 && (
+        <div css={thumbnailContainerCss} data-testid='gallery-thumbnails'>
+          {images.map(({ src, alt }, index) => (
+            <LazyLoadImage
+              key={index}
+              src={src}
+              alt={alt}
+              width={'100%'}
+              effect='blur'
+              data-selected={picturedShown === index}
+              data-testid='gallery-image'
+              onClick={() => setPicturedShown(index)}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Gallery;
