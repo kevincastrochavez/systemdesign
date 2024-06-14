011aaabb198cc66a3822a711b708b643e2ea8b48
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-06-14 13:24:53 -0600

    fixed width for the icon component

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -9,6 +9,7 @@ const iconContainerCss = css`
   place-items: center;
   padding: 8px;
   border-radius: 100px;
+  width: fit-content;
   box-shadow:
     rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
     rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
