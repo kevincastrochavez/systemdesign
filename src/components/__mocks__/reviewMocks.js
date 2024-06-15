33aee0e78ec2f8ded83a5cc35bb32aebd01a3f4d
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-06-15 14:01:16 -0600

    added storybook story for the review component, along with mocks

---
+++ src/components/__mocks__/reviewMocks.js
@@ -0,0 +1,14 @@
+import picture from '../../assets/kevin.jpg';
+
+const reviewDefault = {
+  name: 'Kevin Castro',
+  rating: 3.5,
+  date: 'January 1, 2022',
+  text: "I've worn it everywhere, super durable and fashionable",
+  avatar: {
+    src: picture,
+    alt: 'Kevin Castro at the Sand Dunes',
+  },
+};
+
+export { reviewDefault };
---
+++ src/components/review/Review.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta } from '@storybook/react';
+
+import Review from './Review';
+import { ReviewProps } from '../../utils/types';
+import { reviewDefault } from '../__mocks__/reviewMocks';
+
+// eslint-disable-next-line storybook/story-exports
+const meta: Meta<typeof Review> = {
+  title: 'Components/Review',
+  component: Review,
+  argTypes: {
+    rating: {
+      control: { type: 'range', min: 0, max: 5, step: 0.5 },
+    },
+  },
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Basic = (args: ReviewProps) => <Review {...args} />;
+Basic.args = {
+  ...reviewDefault,
+};
