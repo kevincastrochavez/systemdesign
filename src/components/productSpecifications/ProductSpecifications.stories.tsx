688212a6e912c800cb1ae7819e6353601d532486
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-06-17 21:38:39 -0600

    added storybook story and mocks for such component

---
+++ src/components/productSpecifications/ProductSpecifications.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta } from '@storybook/react';
+
+import ProductSpecifications from './ProductSpecifications';
+import { ProductSpecificationsProps } from '../../utils/types';
+import { productSpecificationsDefault } from '../__mocks__/productSpecificationsMocks';
+
+// eslint-disable-next-line storybook/story-exports
+const meta: Meta<typeof ProductSpecifications> = {
+  title: 'Components/ProductSpecifications',
+  component: ProductSpecifications,
+  argTypes: {},
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Basic = (args: ProductSpecificationsProps) => (
+  <ProductSpecifications {...args} />
+);
+Basic.args = {
+  ...productSpecificationsDefault,
+};
